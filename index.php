33017610924c7aa5dd5e234cd02e21052df36078
SKM Ethereal <ethereal97@gmail.com>
2018-09-07 06:18:14 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<html>
+<head>
+   <title>PHP - INFO </title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1.0">
+<head>
+
+<body>
+
 <?php
-  echo "<h2>It's work!!</h2>";
+   $USERID = $_POST['usn'];
+   if ($USERID) {
+       echo "<p>Welcome back, $USER";
+   } else {
+       echo "<form action='' method='POST' style='width=100%;padding:15px 28px;margin:auto'> <input type='text' placeholder='USERNAME' name='usn' style='display:block;width:100%;padding:12px 18px'> <input type='password' placeholder='PASSWORD' name='pwd' style='display:block;width:100%;padding:12px 18px'> <input type='submit' value='LOGIN' style='display:block;width:70%;padding:12px 18px;text-align:auto;background:#2569df;'></form>";
+   }
 ?>
+
+</body>
+</html>
