772d0733cebd0e5ad2a700eb1f8700d356db9106
ETHERE.AI <ethereal97@gmail.com>
2018-09-10 15:23:04 +0630

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <?php
    $USERID = $_POST['usn'];
    if ($USERID) {
-       echo "<p>Welcome back, $USER";
+       echo "<p>Welcome back, $USERID";
    } else {
        echo "<form action='' method='POST' style='width=100%;padding:15px 28px;margin:auto'> <input type='text' placeholder='USERNAME' name='usn' style='display:block;width:100%;padding:12px 18px'> <input type='password' placeholder='PASSWORD' name='pwd' style='display:block;width:100%;padding:12px 18px'> <input type='submit' value='LOGIN' style='display:block;width:70%;padding:12px 18px;text-align:auto;background:#2569df;'></form>";
    }
