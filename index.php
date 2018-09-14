4e5d960246637a3656407297fd947322ddb63e7a
ETHERE.AI <ethereal97@gmail.com>
2018-09-14 17:12:37 +0630

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,22 @@
    header("Access-Control-Allow-Origin: *");
 ?>
 
+<!DOCTYPE html>
 <html>
+
 <head>
-   <title>PHP - INFO </title>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1.0">
-<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Live</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
 
 <body>
+    <div id="app"></div>
 
-<?php
-   $USERID = $_POST['usn'];
-   if ($USERID) {
-       echo "<p>Welcome back, $USERID";
-   } else {
-       echo "<form action='' method='POST' style='width=100%;padding:15px 28px;margin:auto'> <input type='text' placeholder='USERNAME' name='usn' style='display:block;width:100%;padding:12px 18px'> <input type='password' placeholder='PASSWORD' name='pwd' style='display:block;width:100%;padding:12px 18px'> <input type='submit' value='LOGIN' style='display:block;width:70%;padding:12px 18px;text-align:auto;background:#2569df;'></form>";
-   }
-?>
+    <script src="main.js"></script>
 
 </body>
+
 </html>
